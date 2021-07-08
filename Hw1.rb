5a5f84afb254556de55322f49f010dbdc097369a
xSSinnerx <61522712+xSSinnerx@users.noreply.github.com>
2021-07-08 11:43:51 +0300

    Update Hw1.rb

--- Hw1.rb
+++ Hw1.rb
@@ -34,5 +34,16 @@ def count_positives_sum_negatives(lst)
 
   #task 3 (Kata 7)
 
+def locker_run x
+  (1..Math.sqrt(x)).map { |i| i * i}
+end
+
+def run_task
+  print 'Enter total number of lockers:'
+  x = gets.chomp.to_i
+  puts "Here is an array filled with the locker numbers of
+  those which are open at the end of his run : #{locker_run(x)}"
+end
+
   
   
