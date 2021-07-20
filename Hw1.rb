1a6e7d8bf69ad36ebe162c001167faab73a198f7
xSSinnerx <61522712+xSSinnerx@users.noreply.github.com>
2021-07-20 11:04:06 +0300

    Update Hw1.rb

--- Hw1.rb
+++ Hw1.rb
@@ -29,5 +29,26 @@ def run_task
   those which are open at the end of his run : #{locker_run(x)}"
 end
 
+#task 4 Kata 6 Rainfall
+
+def mean(town, strng)
+   return -1 if (numbers=extract_data(town, strng)).nil?
+   numbers.sum/12
+end
+
+def variance(town, strng)
+  return -1 if (numbers=extract_data(town, strng)).nil?
+  mean = numbers.sum/12
+  variance =numbers.map {|x| (x-mean)**2} .sum/12
+end
+
+def extract_data(town,strng)
+  data_hash = strng.split("\n").map{ |str| str.split(":") }.to_h
+  return nil if data_hash[town].nil?
+  numbers = data_hash[town].scan(/\d+.\d+/).map(&:to_f)
+end
+
+# task 5 Kata 5
+
   
   
