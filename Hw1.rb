aef1f6cc56f7d04809367fafd6e898c8c3d5929a
xSSinnerx <61522712+xSSinnerx@users.noreply.github.com>
2021-07-20 19:12:13 +0300

    Update Hw1.rb

--- Hw1.rb
+++ Hw1.rb
@@ -50,5 +50,24 @@ end
 
 # task 5 Kata 5
 
+
+def gap(g, m, n)
+  numbers = (m).upto(n-1).select { |b| b.odd? }
+  pair = nil
+  numbers.each do |number|
+    next unless is_prime?(number)
+    if is_prime?(number + g) && !(number + 1).upto(number + g - 1).any? { |b| is_prime?(b) }
+      pair = [number, number + g]
+      break
+    end
+  end
+  pair
+end
+
+def is_prime?(number)
+  return false if number <= 1
+  (2..Math.sqrt(number)).each { |i| return false if (number % i).zero? }
+end
+
   
   
